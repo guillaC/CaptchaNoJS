27a29df9afb9c19166185542700ed3b535e48329
Gui <6315083+guillaC@users.noreply.github.com>
2022-06-28 19:35:11 +0200

    maj 1.0.1

--- CaptchaNoJS/Captcha.cs
+++ CaptchaNoJS/Captcha.cs
@@ -15,7 +15,7 @@ namespace CaptchaNoJS
         ///<code>
         ///Captcha cptch = new CaptchaNoJS.Captcha(350, 50, 15);
         ///</code>
-        /// </example>
+        ///</example>
         ///</summary>
 
         public Captcha(int pWidth, int pHeight, int pLength)
@@ -36,21 +36,54 @@ namespace CaptchaNoJS
         /// <see href="https://goonlinetools.com/html-viewer/#jaii4893duk2b3ztkcem">see the result</see>
         /// </example>
         /// </summary>  
-        public string GenerateAsB64()
+        public string GenerateAsB64(CaptchaType captchaType = CaptchaType.Line)
         {
             MemoryStream result = new MemoryStream();
-            canvas.FillColor = getRandomColor();
+            canvas.FillColor = GetRandomColor();
 
-            for (int i = 0; i < 30; i++)
+            switch (captchaType)
             {
-                canvas.StrokeColor = getRandomColor();
-                canvas.StrokeSize = random.Next(2,10);
-                canvas.DrawLine(random.Next(bmp.Width), random.Next(bmp.Height), random.Next(bmp.Width), random.Next(bmp.Height));
+                case CaptchaType.Line:
+                    AddLines(30);
+                    break;
+                case CaptchaType.Circle:
+                    AddCircles(30);
+                    break;
+                case CaptchaType.Random:
+
+                    if (random.Next(0, 100) > 50)
+                    {
+                        AddLines(30);
+                    }
+                    else
+                    {
+                        AddCircles(300);
+                    }
+                    break;
             }
 
             canvas = AddStringToImg(canvas);
             bmp.WriteToStream(result);
             return Convert.ToBase64String(result.ToArray());
+        }
+
+        private void AddLines(int nb)
+        {
+            for (int i = 0; i < nb; i++)
+            {
+                canvas.StrokeColor = GetRandomColor();
+                canvas.StrokeSize = random.Next(2, 10);
+                canvas.DrawLine(random.Next(bmp.Width), random.Next(bmp.Height), random.Next(bmp.Width), random.Next(bmp.Height));
+            }
+        }
+        private void AddCircles(int nb)
+        {
+            for (int i = 0; i < nb; i++)
+            {
+                canvas.StrokeColor = GetRandomColor();
+                canvas.StrokeSize = random.Next(2, 10);
+                canvas.DrawCircle(new PointF(random.Next(bmp.Width), random.Next(bmp.Height)), random.Next(2, 20));
+            }
         }
 
         /// <summary>  
@@ -69,7 +102,7 @@ namespace CaptchaNoJS
             for (int i = 0; i < answer.Length; i++)
             {
                 pCanvas.StrokeColor = Colors.White;
-                pCanvas.FontColor = getRandomColor();
+                pCanvas.FontColor = GetRandomColor();
                 pCanvas.Font = new Font("Arial");
                 pCanvas.FontSize = random.Next(20, 25);
                 pCanvas.DrawString(answer[i].ToString(), strLoc.X, strLoc.Y, HorizontalAlignment.Right);
@@ -85,9 +118,16 @@ namespace CaptchaNoJS
             return new string(Enumerable.Repeat(chars, length).Select(s => s[random.Next(s.Length)]).ToArray());
         }
 
-        private Color getRandomColor()
+        private Color GetRandomColor()
         {
             return new Color(random.Next(0, 255), random.Next(0, 255), random.Next(0, 255));
+        }
+
+        public enum CaptchaType
+        {
+            Line,
+            Circle,
+            Random
         }
     }
 }
